fb82f4e105e4c13aa2a87b6d8fc73e530ce2441f
flrrub <kangjoonwei@gmail.com>
2024-10-27 01:31:37 +0800

    updates

--- pdf/mari_reader.py
+++ pdf/mari_reader.py
@@ -1,37 +1,47 @@
 import pdfplumber
 import pandas as pd
 import os
+import re
 
-filepath = os.getcwd()
-filedir = os.path.join(filepath,'posb.pdf')
+filepath = '/home/flrrub/Documents'
+filedir = os.path.join(filepath,'Oct2024.pdf')
+
+#posb = os.path.join(filepath,'posb.pdf')
+
+
+    
+#%% Open the PDF file with pdfplumber
+# with pdfplumber.open(pdf_file) as pdf:
+#     # Initialize an empty DataFrame to hold all tables
+#     all_tables = pd.DataFrame()
+
+#     # Loop through the pages (from 2nd to the second-last page)
+#     for i in range(1, len(pdf.pages) - 1):  # 1 is for the 2nd page, and len()-1 for the 2nd-last page
+#         page = pdf.pages[i]
 
 
 #%%
+pattern = r"([A-Za-z0-9\s*\/\-\|\.]+)\n(\d{2} \w{3}) (\d{2} \w{3}) (-?\d+\.\d{2})"
+transactions = []
+
 with pdfplumber.open(filedir) as pdf:
-    all_tables = pd.DataFrame()
-    
-    for i in range(1, len(pdf.pages) - 1):
-        page = pdf.pages[i]
-        
-        table = page.extract_table()
-        
-        if table:
-            df = pd.DataFrame(table)
-            all_tables = pd.concat([all_tables, df], ignore_index=True)
+    page = pdf.pages[1]
+    cropped_page = page.within_bbox((40,205,553,743))
+    text = cropped_page.extract_text()
+  
+matches = re.findall(pattern, text)
 
-print(all_tables)
+for match in matches:
+    description, posted_date, transaction_date, amount = match
+    description = re.sub(r"\s*(Card Payment|Instant Checkout)\s*", "", description).strip()
+    transactions.append({
+        "Transaction Date": transaction_date,
+        "Posting Date": posted_date,
+        "Description": description,
+        "Amount": float(amount)
+    })
+
+df = pd.DataFrame(transactions)
 
 
-#%%
-with pdfplumber.open(filedir) as pdf:
-    all_tables = pd.DataFrame()
-    
-    page = pdf.pages[2]
-    table = page.extract_table()
-    if table:
-        df = pd.DataFrame(table)
-        all_tables = pd.concat([all_tables, df], ignore_index=True)
-        
-    
-    print(all_tables)
-    
+
