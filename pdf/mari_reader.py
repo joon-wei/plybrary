72b2fec8140317ac3f9de09122965bbf3be2039a
Joon Wei <kangjoonwei@gmail.com>
2024-10-27 21:39:56 +0800

    mari reader updates

--- pdf/mari_reader.py
+++ pdf/mari_reader.py
@@ -55,28 +55,18 @@ def read_mari():
     return transactions
 
 
-# 595 842 total resolution 
-# (40,204,553,743) works for page 1 
-# (40,125,553,743) seems to work for page 3
-
-
 # Processing starts
 file_name = 'Oct 2024' 
 
 file_path = os.path.join(os.getcwd(),'bank_statements')
-if not os.path.exists(file_path):
-    os.makedirs(file_path) # place PDF statement here
-
 file_dir = os.path.join(file_path,f'{file_name}.pdf') # main dir of the pdf statement
 csv_file_name = f'Mari_{file_name}.csv'
 
 transactions = read_mari()
 df = pd.DataFrame(transactions)
+df['Amount'] = df['Amount'] * -1
 
 # write to csv file
 with open(os.path.join(file_path,csv_file_name), 'w', newline = '') as file:
     file.write(file_name + "\n")
     df.to_csv(file, index=False)
-
-
-
