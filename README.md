40c3e9278ee2124dcc591f07bde851f81d7de448
Joon Wei <kangjoonwei@gmail.com>
2025-02-01 00:29:54 +0800

    updates to crypto

--- README.md
+++ README.md
@@ -10,23 +10,32 @@ from modules import {module_name}
 ### dabase.db and crypto data
 Checkout crypto_demo.py in the new crypto folder. This script uses a new module from modules called 'database' which at the moment contains functions to download crypto data using ccxt module, stores and extracts data from da base
 Only 4 parameters needed to set by the user: coin symbol, timeframe, start_time and end_time. 
-Da base contains different tables meant for different timeframes. I decided to separate it this way since I rather rely on straight downloaded data from the exchange as upposed to storing the maximum granular data (1m intervals) 
-into a single massive table, and creating my own SQL aggregation queries to extract more wider intervals, which in quirky SQL fashion, is not as simple as it is seems. You can catch a glimpse of my testing of that in
-plybrary>database>queries.sql. Of course, if you work with alot of smaller timeframe data, then that could be a better option for you.
 
 crypto_demo.py contains all the necessary functions to download, store, and extract the data that you need for your analysis. 
-download_crypto_data downloads the ohlcv data and stores it into a dataframe in yfinance fashion.
-store_crypto_data function will store it into the appropriate table in da base without any overlapping data, using Timestamp and Symbol as primary keys, meaning any new data with the same symbol and time as the existing table would be skipped
+*download_crypto_data downloads the ohlcv data and stores it into a dataframe in yfinance fashion.
+*store_crypto_data function will store it into the appropriate table in da base without any overlapping data, using Timestamp and Symbol as primary keys, meaning any new data with the same symbol and time as the existing table would be skipped
 to avoid duplicate rows.
-pull_crypto_data simply pulls the data accordingly to which coin, timeframe and time set, with a limit of 1000 rows by default.
+*pull_crypto_data simply pulls the data accordingly to which coin, timeframe and time set, with a limit of 1000 rows by default.
 
-## Current Projects:
+Basic simulation functions are also done which including adding long and short positions with TP and SL. Much more to be expanded including using leverage, combining with the charting etc..
+These would be moved into a simulation module in the future so it can be used with both stock and crypto (and others) analysis. 
+
+### unimportant squabbles
+Da base contains different tables meant for different timeframes. I decided to separate it this way since I rather rely on straight downloaded data from the exchange as upposed to storing the maximum granular data (1m intervals) 
+into a single massive table, and creating my own SQL aggregation queries to extract more wider intervals, which in quirky SQL fashion, is not as simple as it is seems. You can catch a glimpse of my testing of that in
+plybrary>database>queries.sql. Of course, if working with smaller timeframe data is more ideal for the user, then that could be a better option.
+
+
+### WIP projects
+Equity analysis, which would be the same thing I have done for crypto. Ideally the data will be formatted standardised with the crypto stuff so that all modules can be used for both equity and crypto analysis.
+
+## Other Projects:
 ### Stonks
 Portfolio analysis. Key in the tickers, amount purchased and purchase date into the respective list. 
 Includes some basic plots of information like price performance overtime and weighted returns.
 
 ### Casino
-Included my toto results webscraper for the lols. Probably will expand in more gambling related scripts in the future.
+Included my toto results webscraper which pairs with the excel file for analysis on most frequent/least frequent numbers. 
 
 ## Modules included:
 ### stock_analysis
@@ -39,5 +48,5 @@ PDF readers will be stored here. Some banks do not give statements in excel or c
 hence will be focusing on scripts to read bank statements to store transactions into DataFrames for easy record keeping. 
 Will explore ways of storing these records into a database file which will allow us to create some graphs and simple analysis on spending habits overtime.
 
-### Future planned aditions
+## Future planned aditions
 Options data on equity
--- crypto/crypto_demo.py
+++ crypto/crypto_demo.py
@@ -1,7 +1,7 @@
 from modules import database
 import mplfinance as mpf
 import pandas as pd
-# import numpy as np
+import numpy as np
 
 def add_rsi(dataframe, window=14):
     window_length = window
@@ -26,12 +26,11 @@ def add_stoch_rsi(dataframe,window=14):
     dataframe.drop(columns=['RSI_min','RSI_max'], inplace=True)
     dataframe.dropna(subset=['StochRSI'], inplace=True)
     
-
 #%% set ticker and timeframe
-symbol = 'ETH/USDT'
+symbol = 'SOL/USDT'
 timeframe = '1h'
-start_time = '2025-01-29'
-end_time = '2025-01-30'
+start_time = '2025-01-22'
+end_time = '2025-01-29'
 
 start_time_unix = database.create_timecode(start_time)
 end_time_unix = database.create_timecode(end_time)
@@ -47,11 +46,10 @@ print('Insert into table crypto_{} successful'.format(timeframe))
 #%% Pull data from dabase
 data = database.pull_crypto_data(symbol,timeframe,start_time,end_time)
 
-#%% Charting
 data = data.drop(columns=['Symbol','Timezone'])
 data['Timestamp'] = pd.to_datetime(data['Timestamp'])
 data.set_index('Timestamp', inplace=True)
-
+#%% Charting
 add_rsi(data, window=14)
 add_stoch_rsi(data, window=14)
 
@@ -65,6 +63,100 @@ mpf.plot(data,
          title=f'{symbol} | {start_time} to {end_time}',
          mav=(5,20),
          addplot=[rsi_plot, stoch_rsi_plot],
-         panel_ratios=(6,1,1,1),
-         figsize=(10,6)
+         panel_ratios=(3,1,1,1),
+         figsize=(25,15)
          )
+
+#%% Simulate position
+def add_long_sltp(df, trade_size, trade_start, stop_loss = 1, take_profit = 1):
+    df = df.copy()
+    df['trade_position'] = np.nan
+    df['exit_reason'] = pd.Series(dtype='object')
+    
+    if trade_start in df.index:
+        df.loc[trade_start,'trade_position'] = trade_size
+        entry_price = df.loc[trade_start,'Close']
+        print(f'Entry_price: {entry_price}')
+    else:
+        print('trade_start not in data timeframe')
+        return df
+    
+    stop_loss_price = entry_price * (1 - stop_loss)
+    take_profit_price = entry_price * (1 + take_profit)
+    print(f'Stop Loss price: {stop_loss_price}\nTake Profit Price: {take_profit_price}')
+    
+    trade_active = True
+    for date in df.loc[trade_start:].index:
+        if trade_active:
+            price = df.loc[date, 'Close']
+            if price <= stop_loss_price:
+                df.loc[date, 'trade_position'] = 0  # Exit trade
+                df.loc[date, 'exit_reason'] = 'Stop-Loss Triggered'
+                print('Stop-Loss Triggered')
+                trade_active = False  
+            elif price >= take_profit_price:
+                df.loc[date, 'trade_position'] = 0  
+                df.loc[date, 'exit_reason'] = 'Take-Profit Reached'
+                print('Take-Profit Reached')
+                trade_active = False  
+            else:
+                df.loc[date, 'trade_position'] = trade_size
+    
+    df['trade_position'] = df['trade_position'].ffill().fillna(0)
+    
+    # calculate returns
+    df['return'] = df['Close'].pct_change()
+    df['return($)'] = df['return'] * df['trade_position']
+    df['cumulative_position'] = df['trade_position'].where(df['trade_position'] > 0, np.nan)
+    df['cumulative_position'] += df['return($)'].cumsum()
+    
+    return df
+
+def add_short_sltp(df, trade_size, trade_start, stop_loss = 1, take_profit = 1):
+    df = df.copy()
+    df['trade_position'] = np.nan
+    df['exit_reason'] = pd.Series(dtype='object')
+    
+    if trade_start in df.index:
+        df.loc[trade_start,'trade_position'] = trade_size
+        entry_price = df.loc[trade_start,'Close']
+        print(f'Entry_price: {entry_price}')
+    else:
+        print('trade_start not in data timeframe')
+        return df
+    
+    stop_loss_price = entry_price * (1 + stop_loss)
+    take_profit_price = entry_price * (1 - take_profit)
+    print(f'Stop Loss price: {stop_loss_price}\nTake Profit Price: {take_profit_price}')
+    
+    trade_active = True
+    for date in df.loc[trade_start:].index:
+        if trade_active:
+            price = df.loc[date, 'Close']
+            if price >= stop_loss_price:
+                df.loc[date, 'trade_position'] = 0  # Exit trade
+                df.loc[date, 'exit_reason'] = 'Stop-Loss Triggered'
+                print('Stop-Loss Triggered')
+                trade_active = False  
+            elif price <= take_profit_price:
+                df.loc[date, 'trade_position'] = 0  
+                df.loc[date, 'exit_reason'] = 'Take-Profit Reached'
+                print('Take-Profit Reached')
+                trade_active = False  
+            else:
+                df.loc[date, 'trade_position'] = trade_size
+    
+    df['trade_position'] = df['trade_position'].ffill().fillna(0)
+    
+    # calculate returns
+    df['return'] = df['Close'].pct_change() * -1
+    df['return($)'] = df['return'] * df['trade_position']
+    df['cumulative_position'] = df['trade_position'].where(df['trade_position'] > 0, np.nan)
+    df['cumulative_position'] += df['return($)'].cumsum()
+    
+    return df
+
+
+simulation = add_short_sltp(data,100,'2025-01-22 05:00:00', stop_loss=0.1, take_profit=0.10)
+
+
